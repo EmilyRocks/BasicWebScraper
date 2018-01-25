4e06a48df8c68ebf94475133225574eb2c3e4f66
Emily <31497343+EmilyRocks@users.noreply.github.com>
2018-01-25 13:35:39 -0500

    Add project files.

---
+++ ScrapeClient/Program.cs
@@ -0,0 +1,34 @@
+﻿using ScrapeLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScrapeClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Prefer to read the jumbled jargon behind a webpage instead"
+                + " of that ridiculously readable format?");
+
+            Console.WriteLine("Here's the answer to all your problems.");
+
+            Console.WriteLine("Copy a URL here:");
+
+            string userInput = Console.ReadLine();
+
+            Scrape myScrape = new Scrape();
+
+            string value = myScrape.ScrapeWebpage(userInput);
+
+            //sample hardcoded instead of user Input
+            //string value = myScrape.ScrapeWebpage("https://msdn.microsoft.com/en-us/");
+
+            Console.WriteLine(value);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ScrapeLibrary/Scrape.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScrapeLibrary
+{
+
+    public class Scrape
+    {
+        //inserting a method for this new class
+        public string ScrapeWebpage(string url)
+        {
+            return GetWebpage(url);
+        }
+
+        public string ScrapeWebpage(string url, string filepath)
+        {
+            string reply = GetWebpage(url);
+
+            File.WriteAllText(filepath, reply);
+            return reply;
+        }
+
+        //using private helper method to hide the functionality of actually getting the webpage
+        private string GetWebpage(string url)
+        {
+            WebClient client = new WebClient();
+            string content = client.DownloadString(url);
+            content += "THAT'S ALL FOLKS!!!";
+            return content;
+
+
+        }
+    }
+}
+
